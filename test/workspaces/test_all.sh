d2ff1c7cb6efceb691b02620fd32ef77ad05d266
John Cupitt <jcupitt@gmail.com>
2026-03-19 15:48:27 +0000

    add test_all.sh for wses

---
+++ test/workspaces/test_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+should_pass="\
+  test_colour.ws \
+  test_filter.ws \
+  test_histogram.ws \
+  test_image.ws \
+  test_matrix.ws \
+"
+
+should_fail="test_fail.ws"
+
+for test in $should_pass; do
+  echo -n "$test: "
+  if ! snip --test --workspace $test; then
+    echo FAIL
+    exit -1
+  fi
+  echo ok
+done
+
+for test in $should_fail; do
+  echo -n "$test should fail: "
+  if snip --test --workspace $test; then
+    echo FAIL
+    exit -1
+  fi
+  echo ok
+done
+
+echo all tests passed successfully
